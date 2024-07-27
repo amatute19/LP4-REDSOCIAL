2d756608992e9a6560b3d292103b5fe822cf2b3b
amatute19 <amadomatute19@gmail.com>
2024-07-27 16:04:57 -0600

    Update share.jsx

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,9 +1,9 @@
 import "./share.css";
 import {
   PermMedia,
-  Label,
-  Room,
-  EmojiEmotions,
+  // Label,
+  // Room,
+  // EmojiEmotions,
   Cancel,
 } from "@material-ui/icons";
 import { useContext, useRef, useState } from "react";
@@ -78,7 +78,7 @@ export default function Share() {
                 onChange={(e) => setFile(e.target.files[0])}
               />
             </label>
-            <div className="shareOption">
+            {/* <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
             </div>
@@ -89,7 +89,7 @@ export default function Share() {
             <div className="shareOption">
               <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
               <span className="shareOptionText">Feelings</span>
-            </div>
+            </div> */}
           </div>
           <button className="shareButton" type="submit">
             Share
